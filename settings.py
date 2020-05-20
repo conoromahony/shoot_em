2b05cb39ab47dc1dce7e24e1b051638783c8676b
Conor O'Mahony <conchuromathuna@gmail.com>
2020-05-20 17:08:10 -0400

    Shooting Aliens

--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 5
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
